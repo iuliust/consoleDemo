8f0b77999729ebaf0a8e397dc3d026216355ed91
Julien Teyteau <iuliust@gmail.com>
2017-03-15 22:09:19 +0100

    initial version works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Partie1Component } from './partie1/partie1.component';
+import { Partie2Component } from './partie2/partie2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Partie1Component,
+    Partie2Component
   ],
   imports: [
     BrowserModule,
